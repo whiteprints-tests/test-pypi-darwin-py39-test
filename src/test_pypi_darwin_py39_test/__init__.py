dcbd28171e1142390788d9d58e7ef598c5f99af4
Romain Brault <mail@romainbrault.com>
2024-11-16 04:16:36 +0000

    feat: ✨ add command line interface

--- src/test_pypi_darwin_py39_test/__init__.py
+++ src/test_pypi_darwin_py39_test/__init__.py
@@ -6,8 +6,15 @@
 
 from typing import Final
 
+from test_pypi_darwin_py39_test.environment import (
+    ENVIRONMENT_FILE,
+    load_dotenv,
+)
 from test_pypi_darwin_py39_test.package_metadata import __version__
 
 
 __all__: Final = ["__version__"]
 """Public module attributes."""
+
+
+load_dotenv(ENVIRONMENT_FILE)
